d9121ed1acfb5484360bb4c6b116e79cdd8aec16
benjolbawang08 <juntarisnaandrian@gmail.com>
2025-06-23 22:40:57 +0700

    update server.js

--- server.js
+++ server.js
@@ -24,5 +24,5 @@ app.use("/api/users", userRoutes);
 app.use("/api/questions", questionRoutes);
 app.use("/api/results", resultRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
